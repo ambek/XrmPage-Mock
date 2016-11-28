5e5b8e0070c7d2c7d53b9ed0b09e02a1c173af0c
Stefan Schneider <stefan.schneider@connectiv.de>
2016-11-28 17:12:04 +0100

    removed old tests added new tests

--- Attributes/__tests__/Attribute.test.ts
+++ Attributes/__tests__/Attribute.test.ts
@@ -42,3 +42,63 @@ it("Attribute: getIsDirty Is Implemented", () => {
     expect(attr.getIsDirty).not.toBe(null);
 });
 
+it("Attribute: getIsDirty false at start", () => {
+    expect(attr.getIsDirty).toBe(false);
+});
+
+it("Attribute: getIsDirty false at start", () => {
+    expect(attr.getIsDirty).toBe(false);
+});
+
+it("Attribute: getIsDirty true post setValue", () => {
+    attr.setValue("sthsW");
+    expect(attr.getIsDirty).toBe(true);
+});
+
+it("Attribute: setValue Is Implemented", () => {
+    expect(attr.setValue).not.toBe(null);
+});
+
+it("Attribute: getIsDirty false post setValue the same", () => {
+    attr.setValue("newValue");
+    attr.setValue("abc");
+    expect(attr.getIsDirty).toBe(false);
+});
+
+it("Attribute: setValue works", () => {
+    attr.setValue("test");
+    expect(attr.getValue()).toBe("test");
+    expect(attr.getIsDirty).toBe(true);
+});
+
+it("Attribute: setSubmitMode Is Implemented", () => {
+    expect(attr.setSubmitMode).not.toBe(null);
+});
+
+it("Attribute: getSubmitMode works undefined", () => {
+    expect(attr.getSubmitMode()).toBe("always");
+});
+
+it("Attribute: getSubmitMode and setSubmintMode works", () => {
+    attr.setSubmitMode("never");
+    expect(attr.getSubmitMode()).toBe("never");
+});
+
+it("Attribute: setRequiredLevel Is Implemented", () => {
+    expect(attr.setRequiredLevel).not.toBe(null);
+});
+
+it("Attribute: getRequiredLevel Is Implemented", () => {
+    expect(attr.getRequiredLevel).not.toBe(null);
+});
+
+it("Attribute: getRequiredLevel works undefined", () => {
+    expect(attr.getRequiredLevel).toBe("none");
+});
+
+it("Attribute: getRequiredLevel and setRequiredLevel works undefined", () => {
+    attr.setRequiredLevel("required");
+    expect(attr.getRequiredLevel).toBe("required");
+});
+
+
---
+++ Control/__test__/Control.test.ts
@@ -0,0 +1,95 @@
+import { Attribute } from "./../../Attributes/Attribute";
+import { Control, CreateControlWithAttribute } from "./../Control";
+
+
+beforeEach(() => {
+    jest.resetModules();
+    const attr= new Attribute("abr_name", "abc", null, null);
+    const ctrl = new Control(attr, "abr_name", "Label", false, true);
+    (<any>window).attr = attr;
+    (<any>window).ctrl = ctrl;
+});
+
+it("Control: getName Is Implemented", () => {
+    expect(ctrl.getName).not.toBe(null);
+});
+
+it("Control: getName Is Implemented", () => {
+    expect(ctrl.getName()).toBe("abr_name");
+});
+
+it("Control: getVisible Is Implemented", () => {
+    expect(ctrl.getVisible).not.toBe(null);
+});
+
+it("Control: getVisible works true", () => {
+    expect(ctrl.getVisible()).toBe(true);
+});
+
+it("Control: getVisible works false", () => {
+    ctrl.setVisible(false);
+    expect(ctrl.getVisible()).toBe(false);
+});
+
+it("Control: setVisible Is Implemented", () => {
+    expect(ctrl.setVisible).not.toBe(null);
+});
+
+it("Control: setVisible works", () => {
+    ctrl.setVisible(false);
+    expect(ctrl.getVisible()).toBe(false);
+    ctrl.setVisible(true);
+    expect(ctrl.getVisible()).toBe(true);
+});
+
+it("Control: getLabel Is Implemented", () => {
+    expect(ctrl.getLabel).not.toBe(null);
+});
+
+it("Control: getLabel works", () => {
+    expect(ctrl.getLabel()).toBe("Label");
+});
+
+it("Control: setLabel Is Implemented", () => {
+    expect(ctrl.setLabel).not.toBe(null);
+});
+
+it("Control: setVisible works", () => {
+    ctrl.setLabel("Label_2");
+    expect(ctrl.getLabel()).toBe("Label_2");
+});
+
+it("Control: getAttribute Is Implemented", () => {
+    expect(ctrl.getAttribute).not.toBe(null);
+});
+
+it("Control: getAttribute works", () => {
+    expect(ctrl.getAttribute()).toBe(attr);
+});
+
+it("Control: getDisabled Is Implemented", () => {
+    expect(ctrl.getDisabled).not.toBe(null);
+});
+
+it("Control: setDisabled Is Implemented", () => {
+    expect(ctrl.setDisabled()).not.toBe(null);
+});
+
+it("Control: setDisabled and getDisabled works", () => {
+    ctrl.setDisabled(true);
+    expect(ctrl.getDisabled()).toBe(true);
+});
+
+it("Control: CreateControlWithAttribute is implemented", () => {
+    expect(CreateControlWithAttribute).not.toBe(null);
+});
+
+it("Control: CreateControlWithAttribute is implemented", () => {
+    var ctrl2 = CreateControlWithAttribute("abr_name", "abc", "none", "Label", false, true);
+    var attr2 = ctrl2.getAttribute();
+    expect(attr2.getValue()).toBe("abc");
+    expect(attr2.getSubmitMode()).toBe("dirty");
+    expect(attr2.getRequiredLevel()).toBe("none");
+    expect(ctrl2.getLabel()).toBe("Label");
+    expect(ctrl2.getName()).toBe("abr_name");
+});
